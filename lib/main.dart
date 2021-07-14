216df08ef7cb25c0493ff9f73d1bfa606867d45a
sk25469 <sk25469@gmail.com>
2021-07-15 01:20:12 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: const Center(
+        child: Text('Widget Playground!'),
+      ),
+    );
+  }
+}
