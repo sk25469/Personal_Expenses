9059e1f755ff553f1662e91ec9e77844f780a1d0
sk25469 <sk25469@gmail.com>
2021-07-15 14:46:14 +0530

    created the transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './Model/Transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,14 +16,40 @@ class MyApp extends StatelessWidget {
 
 // ignore: use_key_in_widget_constructors
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      amount: 69.420,
+      id: "1",
+      date: DateTime.now(),
+      title: "My new phone",
+    ),
+    Transaction(
+      amount: 420.69,
+      id: "2",
+      date: DateTime.now(),
+      title: "My new mechanical keyboard",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: const Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Card(
+            // ignore: sized_box_for_whitespace
+            child: Container(
+              width: double.infinity,
+              child: Text("Chart"),
+            ),
+            elevation: 6,
+          ),
+          Card(
+            child: Text("List of TXN"),
+          ),
+        ],
       ),
     );
   }
