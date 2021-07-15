236a77b976c78b63473627746da1bc4972c5af91
sk25469 <sk25469@gmail.com>
2021-07-15 19:26:16 +0530

    some more visual changes

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,12 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Card(
             // ignore: sized_box_for_whitespace
             child: Container(
+              color: Colors.red,
               width: double.infinity,
               // ignore: prefer_const_constructors
               child: Text("Chart"),
@@ -55,19 +57,45 @@ class MyHomePage extends StatelessWidget {
                   (tx) => Card(
                     child: Row(
                       children: <Widget>[
+                        // amount container
                         Container(
                           margin: const EdgeInsets.symmetric(
                             horizontal: 20,
                             vertical: 20,
                           ),
-                          child: Text(tx.amount.toString()),
-                          decoration: BoxDecoration(border: Border.all()),
+                          child: Text(
+                            tx.amount.toString(),
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
                           padding: const EdgeInsets.all(5),
                         ),
+                        // the title and time
                         Column(
                           children: <Widget>[
-                            Text(tx.title),
-                            Text(tx.date.toString()),
+                            Text(
+                              tx.title,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
                           ],
                         ),
                       ],
