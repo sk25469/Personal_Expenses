bfab3e01577fd1f26113cc7d32d2db81ebed26e0
sk25469 <sk25469@gmail.com>
2021-07-27 15:54:13 +0530

    converted to listview

--- lib/Widgets/transactions_list.dart
+++ lib/Widgets/transactions_list.dart
@@ -8,60 +8,63 @@ class TransactionList extends StatelessWidget {
   const TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // ignore: prefer_const_constructors
-      children: transactions
-          .map(
-            (tx) => Card(
-              child: Row(
-                children: <Widget>[
-                  // amount container
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 20,
+    // ignore: sized_box_for_whitespace
+    return Container(
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                // amount container
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  child: Text(
+                    'Rs ${transactions[index].amount}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
                     ),
-                    child: Text(
-                      'Rs ${tx.amount}',
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(5),
+                ),
+                // the title and date
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
                       style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purple,
+                        fontSize: 16,
+                        color: Colors.black,
                       ),
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: const TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
                       ),
                     ),
-                    padding: const EdgeInsets.all(5),
-                  ),
-                  // the title and date
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: const TextStyle(
-                          fontSize: 10,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
-          )
-          .toList(),
+          );
+        },
+        itemCount: transactions.length,
+        // ignore: prefer_const_constructors
+      ),
     );
   }
 }
