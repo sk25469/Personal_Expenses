2e2af68e7d7ed2055ff2ed0f3adb2355be93e1ae
sk25469 <sk25469@gmail.com>
2021-08-01 11:39:14 +0530

    chart now working

--- lib/Widgets/chart.dart
+++ lib/Widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/Model/Transaction.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses/Widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransaction;
@@ -10,7 +11,7 @@ class Chart extends StatelessWidget {
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
-      var totalSum = 0.0;
+      double totalSum = 0.0;
 
       for (var i = 0; i < recentTransaction.length; i++) {
         if (recentTransaction[i].date.day == weekDay.day &&
@@ -19,16 +20,34 @@ class Chart extends StatelessWidget {
           totalSum += recentTransaction[i].amount;
         }
       }
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
       elevation: 6,
-      margin: EdgeInsets.all(15),
-      child: null,
+      margin: const EdgeInsets.all(15),
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(
+              label: data['day'].toString(),
+              spendingAmt: data['amount'] as double,
+              spendingPctofTotal: totalSpending == 0.0
+                  ? 0.0
+                  : (data['amount'] as double) / totalSpending);
+        }).toList(),
+      ),
     );
   }
 }
---
+++ lib/Widgets/chart_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmt;
+  final double spendingPctofTotal;
+
+  ChartBar(
+      {required this.label,
+      required this.spendingAmt,
+      required this.spendingPctofTotal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Rs. ${spendingAmt.toStringAsFixed(0)}'),
+        const SizedBox(height: 5),
+        // ignore: sized_box_for_whitespace
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.5),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctofTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(label),
+      ],
+    );
+  }
+}
